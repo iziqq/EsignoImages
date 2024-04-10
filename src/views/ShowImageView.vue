44d1b308258b1a433d625caea43fe8b07fa86e08
Libor Fridrich <l.fridrich@esigno.com>
2024-04-10 13:41:33 +0200

    edit images

--- src/views/ShowImageView.vue
+++ src/views/ShowImageView.vue
@@ -1,37 +1,29 @@
 <script lang="ts">
 import { Component, Vue } from "vue-facing-decorator";
-import esignoFooterLogo from "@/assets/esignoFooterLogo.png"
-import esignoLogo from "@/assets/esignoLogo.png"
-import linkedInLogo from "@/assets/linkedInLogo.png"
-import pageIcon from "@/assets/pageIcon.png"
 @Component({
   components: { 
  }
 })
 export default class ShowImageView extends Vue {
-  footerLogo = esignoFooterLogo;
-  esignoLogo = esignoLogo;
-  linkedInLogo = linkedInLogo;
-  pageIcon = pageIcon;
 
 }
 </script>
 
 <template>
   <img 
-    :src="footerLogo" 
+    src="../../esignoFooterLogo.png" 
     alt="footerLogo"
   >
   <img 
-    :src="esignoLogo" 
+    src="../../esignoLogo.png" 
     alt="esignoLogo"
   >
   <img 
-    :src="linkedInLogo" 
+    src="../../linkedInLogo.png" 
     alt="linkedInLogo"
   >
   <img 
-    :src="pageIcon" 
+    src="../../pageIcon.png" 
     alt="pageIcon"
   >
 </template>
